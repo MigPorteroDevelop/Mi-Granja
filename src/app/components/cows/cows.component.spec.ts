bd3f9d16012f493d2bb7c3ccca5528341133548b
migporuiz@gmail.com <58771720+MigPorteroDevelop@users.noreply.github.com>
2020-12-17 21:50:02 +0100

    testeo

--- src/app/components/cows/cows.component.spec.ts
+++ src/app/components/cows/cows.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsersComponent } from './cows.component';
+import { CowsComponent } from './cows.component';
 
-describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+describe('CowsComponent', () => {
+  let component: CowsComponent;
+  let fixture: ComponentFixture<CowsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [ CowsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UsersComponent);
+    fixture = TestBed.createComponent(CowsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
